f80ff9d3b4558110e46ed4c37c58ee069d42c79d
annafukuzaki <aaannnnnnaaa60@gmail.com>
2020-01-09 11:51:52 +0900

    from the first page to the first half of lesson9-5

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -18,6 +18,9 @@ class GroupsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
   def update
     if @group.update(group_params)
       redirect_to root_path, notice: 'グループを更新しました'
